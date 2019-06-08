1cc61bea55b070620cb0c801ea82827a59284a51
Kian-Aghaei <37697445+Kian-Aghaei@users.noreply.github.com>
2019-06-08 16:16:25 +0200

    Adding Header component (#1)
    * initializing the project & putting header up there
    * refactor component structure
    * removing idea folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../../css/main.css';
+import '../../css/base.css';
+import '../../css/fonts.css';
+import '../../css/vendor.css';
+// import './css/font-awesome/css/font-awesome.css'
+import HeaderLogo from './HeaderLogo'
+import Social from './Social'
+import Menu from './Menu'
+import Search from './Search'
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <section className="s-pageheader s-pageheader--home">
+                <header className="header">
+                    <div className="header__content row">
+
+                        <HeaderLogo/>
+                        <Social/>
+                        <Search/>
+
+                        {/*<div className="header__search">*/}
+
+                            {/*<form role="search" method="get" className="header__search-form" action="#">*/}
+                                {/*<label>*/}
+                                    {/*<span className="hide-content">Search for:</span>*/}
+                                    {/*<input type="search" className="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off"></input>*/}
+                                {/*</label>*/}
+                                {/*<input type="submit" className="search-submit" value="Search"></input>*/}
+                            {/*</form>*/}
+
+                            {/*<a href="#0" title="Close Search" className="header__overlay-close">Close</a>*/}
+
+                        {/*</div>*/}
+
+
+                        <a className="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
+
+                        <Menu/>
+                    </div>
+                </header>
+            </section>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/Header/HeaderLogo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../../images/logo.svg'
+
+class HeaderLogo extends React.Component {
+    render() {
+        return(
+            <div className="header__logo">
+                <a className="logo" href="index.html">
+                    <img src={Logo} alt="Homepage"></img>
+                </a>
+            </div>
+        );
+    }
+}
+
+export default HeaderLogo;

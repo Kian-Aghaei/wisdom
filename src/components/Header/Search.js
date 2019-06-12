fc819cd5a4d55137abb3a026a6343a3abb9e352a
Kian Aghaei <k.aghaei@pm.me>
2019-06-12 16:04:26 +0200

    add animation the search box

--- src/components/Header/Search.js
+++ src/components/Header/Search.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
 class HeaderSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            square: '',
+            close: '',
+            visibility: 'visible'
+        };
+    }
+
+    toggleSearch = () => {
+        if (this.state.square === '') {
+            this.setState({
+                square: 'square',
+                close: 'close',
+                visibility: 'hidden'
+            })
+        } else {
+            this.setState({
+                square: '',
+                close: '',
+                visibility: 'visible',
+            })
+        }
+    };
+
     render() {
         return (
-            <div>
-                <a className="header__search-trigger" href="#0"></a>
-            </div>
+            <form className="" id="searchForm">
+                <p className={`searchIndex ${this.state.visibility}`}>SEARCH</p>
+                <input type="text" name="input" className={`input ${this.state.square}`} id="search-input"></input>
+                <button onClick={this.toggleSearch} type="reset" className={`search ${this.state.close}`} id="search-btn"></button>
+            </form>
         );
     }
 }

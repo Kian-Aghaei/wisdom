2dca77a7d47b5ecfe3c998f901edeff87dabac2f
Kian Aghaei <k.aghaei@pm.me>
2019-06-27 16:58:50 +0200

    adding sidebar component directly into the App in order to have overlay

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Header from '../Header/Header';
+import SideMenu from './SideMenu';
 
 function App() {
-  return <Header />;
+  return (
+    <div id="outer-container">
+      <SideMenu className="side-menu" htmlClassName="side-class" bodyClassName="side-class" />
+      <main id="page-wrap">
+        <Header />
+      </main>
+    </div>
+  );
 }
 
 export default App;

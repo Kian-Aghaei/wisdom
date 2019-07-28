777b713f21feac0f2c39a7d2da648a796e7ae7b8
Kian Aghaei <k.aghaei@pm.me>
2019-07-29 00:04:13 +0200

    create an opbject to contain props in headerContentLarge

--- src/components/Header/HeaderContent.js
+++ src/components/Header/HeaderContent.js
@@ -3,45 +3,32 @@ import content from '../../data/HeaderContent.json';
 import HeaderContentLarge from './HeaderContentLarge';
 import HeaderContentSmall from './HeaderContentSmall';
 
-const HeaderContent = () => (
-  <div className="pageheader-content row">
-    <div className="col-full">
-      <div className="featured">
-        <div className="featured__column featured__column--big">
-          <HeaderContentLarge
-            image={
-              content.postDetails.backImage[1]
-            }
-            category={
-              content.postDetails.category[0]
-            }
-            href={
-              content.postDetails.href
-            }
-            date={
-              content.postDetails.date
-            }
-            title={
-              content.postDetails.title[1]
-            }
-            author={
-              content.userDetails.name[0]
-            }
-            authorPage={
-              content.userDetails.href
-            }
-            avatar={
-              content.userDetails.icon[5]
-            }
-          />
-        </div>
-        <div className="featured__column featured__column--small">
-          <HeaderContentSmall />
-          <HeaderContentSmall />
+const HeaderContent = () => {
+  const headerContentLargeProps = {
+    image: content.postDetails.backImage[0],
+    category: content.postDetails.category[0],
+    href: content.postDetails.href,
+    date: content.postDetails.date,
+    title: content.postDetails.title[1],
+    author: content.userDetails.name[0],
+    authorPage: content.userDetails.href,
+    avatar: content.userDetails.icon[5],
+  };
+  return (
+
+    <div className="pageheader-content row">
+      <div className="col-full">
+        <div className="featured">
+          <div className="featured__column featured__column--big">
+            <HeaderContentLarge {...headerContentLargeProps} />
+          </div>
+          <div className="featured__column featured__column--small">
+            <HeaderContentSmall />
+            <HeaderContentSmall />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default HeaderContent;
